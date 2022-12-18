232f1664dea3e7c6510dedd5011d148ecbadf045
behsm <120650251+behsm@users.noreply.github.com>
2022-12-18 12:33:18 -0600

    Update question1.sql

--- inclass_question/question1.sql
+++ inclass_question/question1.sql
@@ -1,1 +1,20 @@
+--1. Calculate total tran_ammt (sum) for each state
+--2. Calculate maximum and minimum tran_ammt on each state and tran_date
+--3. Calculate total transaction which have tran_ammt more than 10000
+--4. Show the state which have total (sum) tran_ammt more than 10000
+--5. show me the states where total ammt is more than 10000
+--6. show me the states where cust_id ='cust_104' and  total ammt is more than 10000
+--7. Calculate total transaction by state [ if state if NULL make it TX] where total transaction is more than 10000
+--8. Show me a message col if state is null then "missing data" else "good data"
+--9. Show me sum of tran_ammt by state [ if state is null and cust_id='cust_104' then 'TX' else 'CA']
+
+
+
+
+--Join Question:
+
+--1.Give me all details from transaction tale and zip_cd from dimension table.
+--2. Sum of tran_ammt by zip_cd
+--3. Give me top 5 customer [ (first name+ last name) is customer] by tran_ammt [highest is first] join on cust_id
+--4. Give me the all cols from tran_fact [ I don't need state_cd is null] first five records [ lower to highest]
 
