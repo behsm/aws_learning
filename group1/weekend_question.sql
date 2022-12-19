0f24f46fba69a2307c3cc1cdb62ebf4aa249ed9b
behsm <120650251+behsm@users.noreply.github.com>
2022-12-19 08:54:10 -0600

    Update weekend_question.sql

--- group1/weekend_question.sql
+++ group1/weekend_question.sql
@@ -12,7 +12,7 @@ FROM cards_ingest.tran_fact
 
 SELECT tran_date, tran_ammt, SUM(tran_ammt)
 OVER (PARTITION BY tran_date) as total_transaction_ammount,
-ROW_NUMBER() over (PARTITION BY tran_date
+DENSE_RANK() over (PARTITION BY tran_date
 				   ORDER BY tran_ammt desc)
 FROM cards_ingest.tran_fact
 ORDER BY tran_date
@@ -46,36 +46,32 @@ ON fact.cust_id = dim.cust_id AND fact.tran_date BETWEEN dim.start_date AND dim.
   
 SELECT *
 FROM ( 
-	SELECT *, SUM(tran_ammt)
-	OVER (PARTITION BY tran_date) as total_transaction_ammount,
-	ROW_NUMBER() over (PARTITION BY tran_date
-	ORDER BY tran_ammt desc) rownumber
-	FROM cards_ingest.tran_fact fact
-	INNER JOIN cards_ingest.cust_dim_details dim 
+	SELECT *, 
+	SUM(tran_ammt) OVER (PARTITION BY tran_date) as total_transaction_ammount,
+	DENSE_RANK() over (PARTITION BY tran_date ORDER BY tran_ammt desc) as ranknum
+	FROM cards_ingest.tran_fact fact INNER JOIN cards_ingest.cust_dim_details dim 
 	ON fact.cust_id = dim.cust_id AND fact.tran_date BETWEEN dim.start_date AND dim.end_date
 	) inq
-WHERE rownumber = 2
+WHERE ranknum = 2
 ORDER BY tran_date
 
 --3. From question 2 : when stat_cd is not euqal to state_cd then data issues else good data as stae_cd_status
  --[Note NUll from left side is not equal NUll from other side  >> means lets sayd NULL value from fact table if compared
  --to NULL Value to right table then it should be data issues]
-
 SELECT *,
 	CASE
-	WHEN stat_cd != state_cd THEN 'data issues'
-	WHEN stat_cd IS NULL THEN 'data issues'
+	WHEN COALESCE(stat_cd,'aa') != COALESCE(state_cd, 'bb') THEN 'data issues'
 	ELSE 'good data'
 	END AS state_cd_status
-FROM ( 
+FROM 
+	( 
 	SELECT *, SUM(tran_ammt)
 	OVER (PARTITION BY tran_date) as total_transaction_ammount,
-	ROW_NUMBER() over (PARTITION BY tran_date
-	ORDER BY tran_ammt desc) rownumber
+	DENSE_RANK() over (PARTITION BY tran_date
+	ORDER BY tran_ammt desc) ranknum
 	FROM cards_ingest.tran_fact fact
 	INNER JOIN cards_ingest.cust_dim_details dim 
 	ON fact.cust_id = dim.cust_id AND fact.tran_date BETWEEN dim.start_date AND dim.end_date
 	) inq
---WHERE rownumber = 2
+WHERE ranknum = 2
 ORDER BY tran_date
- 
