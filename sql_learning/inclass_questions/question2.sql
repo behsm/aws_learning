2b3e8f5d1749413f69f72d50d123df85edb9f6dc
behsm <120650251+behsm@users.noreply.github.com>
2022-12-29 09:42:06 -0600

    Update question2.sql
    optimized 1&2 added questions 3-5

--- sql_learning/inclass_questions/question2.sql
+++ sql_learning/inclass_questions/question2.sql
@@ -13,81 +13,72 @@
 --1. Join  cards_ingest.tran_fact with lkp_state_details on state cd. Make sure if any Null Values from fact remove those records
 --Show me tran_date,state, number of customer per tran_date and state and number of customer company can target for promotion
 --who are not customer in but still lives in the state (population - number of customer)
-
-SELECT cust_id, stat_cd FROM cards_ingest.tran_fact
-WHERE stat_cd is not null
-GROUP BY cust_id, stat_cd
-/*OUTPUT:
-"cust_id","stat_cd"
-"cust_101","NY"
-"cust_102","CA"
-"cust_103","CA"
-"cust_104","TX"
-"cust_107","TX"
-"cust_107","CA"
-"cust_111","NV"
-*/
+--notes from class:
+--join, get unique customers per date, subtract from population count
 
 
-WITH inq1 as (
-SELECT *
+SELECT tran_date, stat_cd, count(distinct cust_id) as num_customers, population_cnt-num_customers as promotion_target_cnt
 FROM cards_ingest.tran_fact tf INNER JOIN lkp_data.lkp_state_details lkp ON tf.stat_cd = lkp.state_cd
-),
-
-inq2 as (
-SELECT tran_date, stat_cd, count(distinct cust_id) as num_customers
-FROM cards_ingest.tran_fact 
 WHERE stat_cd is not NULL
-GROUP BY tran_date, stat_cd
-)
-
-SELECT inq1.tran_date, inq2.stat_cd, inq2.num_customers, population_cnt-num_customers as promotion_target_cnt
-FROM inq1 INNER JOIN inq2 on inq1.tran_date = inq2.tran_date AND inq1.stat_cd = inq2.stat_cd 
-GROUP BY inq1.tran_date, inq2.stat_cd,inq2.num_customers, promotion_target_cnt
+GROUP BY tran_date, stat_cd,lkp.population_cnt
 ORDER BY tran_date, stat_cd
 
-/*OUTPUT:
-"tran_date","stat_cd","num_customers","promotion_target_cnt"
-"2022-01-01","CA","2","498"
-"2022-01-01","NY","1","199"
-"2022-01-01","TX","1","399"
-"2022-01-03","NY","1","199"
-"2022-01-04","CA","2","498"
-"2022-01-05","TX","2","398"
-"2022-02-01","NY","1","199"
-"2022-02-03","NV","1","99"
-"2022-02-03","NY","1","199"
-"2022-02-03","TX","1","399"
-"2022-02-05","CA","1","499"
-"2022-02-05","TX","1","399"
-"2022-03-01","CA","1","499"
-"2022-03-02","CA","1","499"
-"2022-04-01","CA","1","499"
-"2022-07-03","NV","1","99"
-*/
+
+
+
 
 --2. To reach each remaining potential_customer_cnt cost 5$, then show me the states where company has to spend 2nd high $ amount.
 --(make sure do potential_customer_cnt -allready customer count to get remaining potential customer count)
+--on each day what is the 2nd highest amount you have to spend by state & day
+
 WITH inq1 as (
-SELECT *
-FROM cards_ingest.tran_fact tf INNER JOIN lkp_data.lkp_state_details lkp ON tf.stat_cd = lkp.state_cd
-),
+SELECT tf.tran_date, tf.stat_cd,(lkp.potential_customer_cnt-count(distinct cust_id))*5 as promotion_cost, DENSE_RANK() OVER (PARTITION BY tran_date ORDER BY promotion_cost DESC ) AS rnk
+FROM cards_ingest.tran_fact tf INNER JOIN lkp_data.lkp_state_details lkp ON tf.stat_cd = lkp.state_cd 
+WHERE stat_cd is not NULL  --could do rank <=2 if you want the 1st record when there is only 1
+GROUP BY tran_date, stat_cd, potential_customer_cnt
+ORDER BY tran_date, promotion_cost DESC
 
-inq2 as (
-SELECT tran_date, stat_cd, count(distinct cust_id) as num_customers
-FROM cards_ingest.tran_fact 
-WHERE stat_cd is not NULL
-GROUP BY tran_date, stat_cd
 )
+SELECT * 
+FROM inq1
+WHERE rnk <= 2 --could do rank =2 if you dont want 1st cost if its the only one
 
-SELECT inq2.stat_cd, inq2.num_customers, 5*(potential_customer_cnt-num_customers) as cost_to_reach
-FROM inq1 INNER JOIN inq2 on inq1.tran_date = inq2.tran_date AND inq1.stat_cd = inq2.stat_cd 
-GROUP BY inq2.stat_cd,inq2.num_customers, cost_to_reach
-ORDER BY cost_to_reach DESC LIMIT 1 OFFSET 1
+/*
+output:
+"tran_date","stat_cd","promotion_cost","rnk"
+"2022-01-01","TX","1495","1"
+"2022-01-01","CA","990","2"
+"2022-01-03","NY","495","1"
+"2022-01-04","CA","990","1"
+"2022-01-05","TX","1490","1"
+"2022-02-01","NY","495","1"
+"2022-02-03","TX","1495","1"
+"2022-02-03","NY","495","2"
+"2022-02-05","TX","1495","1"
+"2022-02-05","CA","995","2"
+"2022-03-01","CA","995","1"
+"2022-03-02","CA","995","1"
+"2022-04-01","CA","995","1"
+"2022-07-03","NV","445","1"
+*/
 
-/* output:
-  "stat_cd": "TX",
-  "num_customers": "2",
-  "cost_to_reach": "1490"
-*/ 
 
+
+
+
+/*
+3. Same as question 1. But 
+the number of customer from transaction table is total number of unique customer till that date.
+(Hint use window function)
+
+4. Same as question 2. 
+If state cd is NULL  and     COALESCE/CASE 
+cust_id is cust_109 
+then  TX  
+else CA and calculate states where
+company has to spend 2nd lowest $ amount from .
+
+5. Show me the total number of customer company has 
+, total population,
+ and potential_customer_cnt across all the states
+ */
