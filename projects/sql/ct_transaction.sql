c9cc5bbe88cf575b1fc8c76e4f2ad0330ded7c64
behsm <120650251+behsm@users.noreply.github.com>
2023-01-09 06:46:47 -0600

    Update ct_transaction.sql

--- projects/sql/ct_transaction.sql
+++ projects/sql/ct_transaction.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS cards_ingest.tran_fact(
+    tran_id int encode AZ64,
+    cust_id varchar(10) encode raw, 
+    stat_cd varchar(2) encode raw, 
+    tran_ammt decimal(10,2) encode AZ64,
+    tran_date date encode raw
+) SORTKEY(tran_date);
