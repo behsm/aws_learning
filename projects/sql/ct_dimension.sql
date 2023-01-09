a42e096020be56d3e5a58d2e6d4ef23ef273f7a8
behsm <120650251+behsm@users.noreply.github.com>
2023-01-09 06:45:51 -0600

    Update ct_dimension.sql

--- projects/sql/ct_dimension.sql
+++ projects/sql/ct_dimension.sql
@@ -1,5 +1,5 @@
 create table IF NOT EXISTS cards_ingest.cust_dim_details (
-    cust_id varchar(10) encode zstd,
+    cust_id varchar(10) encode raw, 
     state_cd varchar(2) encode zstd,
     zip_cd varchar(5) encode zstd,
     cust_first_name varchar(20) encode zstd,
